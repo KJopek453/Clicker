2f7b05fbe6c59dbdf40a7051f5e5c71d3178f841
Konrad Jopek <90185027+KJopek453@users.noreply.github.com>
2022-02-16 07:01:05 +0100

    Added comments to JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Klasa z parametrami ulepszenia mocy kliknięcia (ile pkt dostajesz za kliknięcie)
 class CPUpgrade{
     constructor(baseCost, additionalCP, numberOfUpgrades, cost){
         this.baseCost=baseCost;
@@ -6,7 +7,7 @@ class CPUpgrade{
         this.cost=cost;
     }
 }
-
+//Klasa z parametrami ulepszenia ile punktów dostajesz na sekundę
 class PPSUpgrade{
     constructor(baseCost, additionalPPS, numberOfUpgrades, cost){
         this.baseCost=baseCost;
@@ -15,7 +16,7 @@ class PPSUpgrade{
         this.cost=cost;
     }
 }
-
+//Tworzenie zmiennych odnoszących się do danych klas oraz id
 const button = document.querySelector('.button');
 const pointsDisplay = document.querySelector('.pointsDisplay');
 const changeToCPUpgrades = document.querySelector('.changeToCPUpgrades');
@@ -26,56 +27,61 @@ const CPUpgrade1 = document.querySelector('.CPUpgrade1');
 const PPSUpgrade1 = document.querySelector('.PPSUpgrade1');
 const CPUpgrade1Text =  document.querySelector('#CPUpgrade1Text');
 const PPSUpgrade1Text =  document.querySelector('#PPSUpgrade1Text');
-
+//Tworzenie zmiennych przechowywujących ilość twoich punktów, moc kliknięcia oraz ilość punktów jaką dostajesz na sekundę
 let pointsCounter = 0, CP = 1, PPS = 0;
-
-let CP1 = new CPUpgrade(100, 1, 1, 0);
-let PPS1 = new PPSUpgrade(100, 1, 1, 0);
+//Stworzenie obiektu dla pierwszych ulepszeń oraz ustawienie ich poczatkowego kosztu
+let CP1 = new CPUpgrade(100, 1, 0, 0);
+let PPS1 = new PPSUpgrade(100, 1, 0, 0);
 CP1.cost=parseInt(CP1.baseCost*Math.pow(1.15, CP1.numberOfUpgrades));
 PPS1.cost=parseInt(PPS1.baseCost*Math.pow(1.15, PPS1.numberOfUpgrades));
-
+//Wczesne ukrycie ulepszeń punktów na sekundę, wyświetlenie początkowe textu pokazującego ilośc punktów oraz kosztu obu ulepszeń
 PPSUpgrades.classList.add('hidden');
+PPSUpgrade1.classList.add('hidden');
 pointsDisplay.textContent = `${pointsCounter} pkt`;
 CPUpgrade1Text.innerHTML = `koszt: ${CP1.cost} pkt`;
 PPSUpgrade1Text.textContent = `koszt: ${PPS1.cost} pkt`;
-
+//zdarzenie pozwalające kupić pierwsze ulepszenie mocy kliknięcia
 CPUpgrade1.addEventListener('click', () => {
-    if(pointsCounter>CP1.cost){
-        CP1.cost=parseInt(CP1.baseCost*Math.pow(1.15, CP1.numberOfUpgrades));
+    if(pointsCounter>=CP1.cost){
         pointsCounter-=CP1.cost;
-        CPUpgrade1Text.innerHTML = `koszt: ${CP1.cost} pkt`;
+        pointsDisplay.textContent = `${pointsCounter} pkt`;
         CP += 1;
         CP1.numberOfUpgrades += 1;
     }
-    
+    CP1.cost=parseInt(CP1.baseCost*Math.pow(1.15, CP1.numberOfUpgrades));
+    CPUpgrade1Text.innerHTML = `koszt: ${CP1.cost} pkt`;
 })
-
-CPUpgrade1.addEventListener('click', () => {
-    PPS1.cost=parseInt(PPS1.baseCost*Math.pow(1.15, PPS1.numberOfUpgrades));
-    if(pointsCounter>PPS1.cost){
+//zdarzenie pozwalające kupić pierwsze ulepszenie punktów na sekundę
+PPSUpgrade1.addEventListener('click', () => {
+    if(pointsCounter>=PPS1.cost){
         pointsCounter-=PPS1.cost;
-        CPUpgrade1Text.innerHTML = `koszt: ${PPS1.cost} pkt`;
+        pointsDisplay.textContent = `${pointsCounter} pkt`;
         PPS += 1;
         PPS1.numberOfUpgrades += 1;
     }
-    
+    PPS1.cost=parseInt(PPS1.baseCost*Math.pow(1.15, PPS1.numberOfUpgrades));
+    PPSUpgrade1Text.innerHTML = `koszt: ${PPS1.cost} pkt`;
 })
-
+//zdarzenie dodające punkty zgodznie z mocą kliknięcia
 button.addEventListener('click', () => {
     pointsCounter += CP;
     pointsDisplay.textContent = `${pointsCounter} pkt`;
 })
-
+//zdarzenie chowające ulepszenia punktów na sekundę i wyświetlająca ulepszenia mocy kliknięcia
 changeToCPUpgrades.addEventListener('click', () => {
     CPUpgrades.classList.remove('hidden');
     PPSUpgrades.classList.add('hidden');
+    CPUpgrade1.classList.remove('hidden');
+    PPSUpgrade1.classList.add('hidden');
 })
-
+//zdarzenie chowające ulepszenia mocy kliknięcia i wyświetlająca ulepszenia punktów na sekundę
 changeToPPSUpgrades.addEventListener('click', () => {
     CPUpgrades.classList.add('hidden');
     PPSUpgrades.classList.remove('hidden');
+    CPUpgrade1.classList.add('hidden');
+    PPSUpgrade1.classList.remove('hidden');
 })
-
+//zdarzenie dodające punkty na sekundę
 function PPSAdd(){
     pointsCounter+=PPS;
     pointsDisplay.textContent = `${pointsCounter} pkt`;
